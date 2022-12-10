3bf7be5b18e5c4c5160c3885736f645c25157c19
mohamedeliwa <mohamed.av11@gmail.com>
2022-12-10 16:20:27 +0200

    using recursion

---
+++ README.md
@@ -0,0 +1,5 @@
+# nth Fibonacci number
+
+simple program to get the fibonacci number at the nth position
+it's built for learning purposes
+> maybe revisited after learning more about Rust
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\nEnter the nth fibonacci position to get its value");
+    println!("The fibonacci squence starts from 0\n");
+
+    // getting the nth position from the user
+    let position: u32 = loop {
+        let mut position = String::new();
+
+        io::stdin()
+            .read_line(&mut position)
+            .expect("Can't read your input");
+
+        let position: u32 = match position.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Pleave input a positive number");
+                continue;
+            }
+        };
+        break position;
+    };
+
+    let value: u64 = nth_fibonacci_value(position);
+    println!(
+        "\nThe fibonacci value at position {} is: {}\n",
+        position, value
+    )
+}
+
+fn nth_fibonacci_value(n: u32) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => nth_fibonacci_value(n - 1) + nth_fibonacci_value(n - 2),
+    }
 }
